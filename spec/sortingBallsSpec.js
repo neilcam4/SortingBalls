30d95748d05f9b0822c3f05c626d01a114e13d23
Neil Campbell <neildcampbell@hotmail.com>
2019-11-15 06:51:32 +0000

    added first test for empty array

---
+++ spec/sortingBallsSpec.js
@@ -0,0 +1,6 @@
+describe("SORTING BALLS", function(){
+    it("should produce a new object with empty array", function(){
+        let newBalls = new Rack()
+        expect(newBalls.balls).toEqual([])
+    })
+})
---
+++ src/sortingBalls.js
@@ -0,0 +1,3 @@
+function Rack(){
+    this.balls = []
+}
