2880bbcdadf1e0bc4e98c5016572c07f7d3f2c99
Neil Campbell <neildcampbell@hotmail.com>
2019-11-15 10:09:02 +0000

    ordered 2 balls into numerical order

--- spec/sortingBallsSpec.js
+++ spec/sortingBallsSpec.js
@@ -3,4 +3,22 @@ describe("SORTING BALLS", function(){
         let newBalls = new Rack()
         expect(newBalls.balls).toEqual([])
     })
+    it("should receive a number and add it to array", function(){
+        let newBalls = new Rack()
+        newBalls.add(10)
+        expect(newBalls.balls).toEqual([10])
+    })
+    it("should receive a new number and add it to array", function(){
+        let newBalls = new Rack()
+        newBalls.add(10)
+        newBalls.add(20)
+        expect(newBalls.balls).toEqual([10, 20])
+    })
+    it("should receive a new number and add it to array that is lower than the first one", function(){
+        let newBalls = new Rack()
+        newBalls.add(10)
+        newBalls.add(5)
+        let result  = newBalls.sort()
+        expect(result).toEqual([5, 10])
+    })
 })
--- src/sortingBalls.js
+++ src/sortingBalls.js
@@ -1,3 +1,23 @@
 function Rack(){
     this.balls = []
 }
+
+Rack.prototype.add = function(number){
+    this.balls.push(number)
+    }
+
+Rack.prototype.sort = function(){
+    newArray = []
+    let a = this.balls[0]
+    console.log(a)
+    let b = this.balls[1]
+    console.log(b)
+    if (a > b){
+        console.log(this.balls.slice(1))
+       newArray = this.balls.slice(1)
+       newArray.push(a)
+       console.log(newArray)
+       
+    }
+    return newArray
+}
