a0101b73a8ae7a7606757cf30960f6b5bc441c9c
Neil Campbell <neildcampbell@hotmail.com>
2019-11-16 05:32:09 +0000

    attempted quick sort

--- src/sortingBalls.js
+++ src/sortingBalls.js
@@ -1,7 +1,8 @@
 function Rack(){
     this.balls = []
     leftIndex = 0;
-    rightIndex = 1
+    rightIndex = 0;
+    console.log(rightIndex)
 }
 
 Rack.prototype.add = function(number){
@@ -14,31 +15,54 @@ Rack.prototype.findMiddle = function(){
     }
 
 Rack.prototype.swap = function(){
+    rightIndex = this.balls.length -1
     let temp = this.balls[leftIndex];
     this.balls[leftIndex] = this.balls[rightIndex];
     this.balls[rightIndex] = temp;
     return this.balls
 }
 
+Rack.prototype.middle = function(){
+    let m =  this.findMiddle()
+    if (this.balls[leftIndex] < m){
+        this.swap()
+        
+    }
+    return this.balls
+}
+// Rack.prototype.partition = function(){
+//     let m = findMiddle()
+//     console.log(m)
+//     let a  = this.balls[leftIndex]
+//     let z = this.balls[rightIndex]
+//     while(a <= z){
+//         while (this.balls[leftIndex] < m){
+//             leftIndex++;
+//         }
+//         while (this.balls[rightIndex >= m]){
+//             rightIndex--;
+//         }
+//         if(this.balls[leftIndex] >= this.balls[rightIndex] ){
+//             // this.balls.swap()
+//             leftIndex ++
+//             rightIndex --
+//         }
+//     }
+//     return this.balls
+// }
+
 Rack.prototype.sort = function(){
     let newArray = []
     let a = 0;
     let A = this.balls[a]
-
     let B = this.balls[a+1]
-
-    
     while(a <= this.balls.length ){
-        // console.log("balls.length = " + this.balls.length)
         if(A > B){
             newArray = this.balls.slice(a+1)
-            // console.log("this.balls after slice = " + this.balls.slice(a+1))
             newArray.push(A)
-            // console.log(newArray)
             a++
         
     };
-    // console.log(newArray)
     return newArray
     }
 }
